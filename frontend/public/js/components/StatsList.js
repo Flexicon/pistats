63b8e97de8e6cdf9724ec06119bd4975bcfec0e0
Mike Repec <mikerepec@gmail.com>
2021-02-27 10:51:33 +0100

    Use 4 column stat layout on larger screens

--- frontend/public/js/components/StatsList.js
+++ frontend/public/js/components/StatsList.js
@@ -3,7 +3,7 @@ import { html } from 'htm/preact';
 import StatsListItem from './StatsListItem';
 
 export default function StatsList({ items = [] }) {
-  return html`<div class="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+  return html`<div class="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
     ${items.map((i) => StatsListItem(i))}
   </div>`;
 }
